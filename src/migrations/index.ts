cb926bc58cf1c1c9ac0bae60cc1cb30c9c231ed3
Galdan Infotech <galdaninfotech@gmail.com>
2026-02-19 07:20:41 +0530

    Fix: migration files

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260218_234554_initial from './20260218_234554_initial';
+import * as migration_20260219_011739_initial from './20260219_011739_initial';
 
 export const migrations = [
   {
-    up: migration_20260218_234554_initial.up,
-    down: migration_20260218_234554_initial.down,
-    name: '20260218_234554_initial'
+    up: migration_20260219_011739_initial.up,
+    down: migration_20260219_011739_initial.down,
+    name: '20260219_011739_initial'
   },
 ];
